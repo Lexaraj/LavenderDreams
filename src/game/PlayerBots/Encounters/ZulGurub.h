8a1c56f94cca93605487a13961313713e5949ec9
Tyler Knox <tyler@knoxy.tk>
2025-08-02 17:55:40 -0400

    Added Zul'Gurub encounter handling for PartyBots

---
+++ src/game/PlayerBots/Encounters/ZulGurub.h
@@ -0,0 +1,38 @@
+#include "PartyBotAI.h"
+#include "PartyBotEncounters.h"
+
+#define MANDOKIR_WATCH 24314
+
+enum EncounterIds
+{
+    TYPE_VENOXIS    = 0,
+    TYPE_JEKLIK     = 1,
+    TYPE_MARLI      = 2,
+    TYPE_THEKAL     = 3,
+    TYPE_ARLOKK     = 4,
+    TYPE_HAKKAR     = 5,
+    TYPE_MADNESS = 6,
+    TYPE_MANDOKIR = 7,
+    TYPE_GAHZRANKA  = 8,
+    TYPE_JINDO      = 9
+};
+    
+class PartyBotEncounters_ZG : public PartyBotEncounters
+{
+public:
+    static bool HandleEncounterAI(PartyBotAI* pBot);
+    
+    static bool VenoxisEncounter(PartyBotAI* pBot);
+    static bool JeklikEncounter(PartyBotAI* pBot);
+    static bool MarliEncounter(PartyBotAI* pBot);
+    static bool ThekalEncounter(PartyBotAI* pBot);
+    static bool ArlokkEncounter(PartyBotAI* pBot);
+    static bool HakkarEncounter(PartyBotAI* pBot);
+    static bool MadnessEncounter(PartyBotAI* pBot);
+    static bool MandokirEncounter(PartyBotAI* pBot);
+    static bool GahzrankaEncounter(PartyBotAI* pBot);
+    static bool JindoEncounter(PartyBotAI* pBot);
+    static bool TrashEncounter(PartyBotAI* pBot);
+
+    static void ResetEncounterVars();
+};
